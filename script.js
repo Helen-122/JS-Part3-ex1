e512ab84f54cc44eb52be35ffb2f5020e35ea92b
Helen-122 <helene.mazourine@gmail.com>
2021-04-11 20:11:43 +0200

    ex1 part 3 JS

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function addBorder(){
+    document.getElementById("rhino").style.border = "thick solid #0000FF";
+}
+function noBorder(){
+    document.getElementById('rhino').style.border ='';
+}
